d74931228bab92cd5566c97de09296db30631f89
Jacob Ingalls <jingalls@uoregon.edu>
2019-03-18 23:55:35 -0700

    deployed the soccer react page, its live

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import { Component } from "react";
+
+const API = "https://www.reddit.com/r/soccerstreams__/.json";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      redditData: [],
+      defaultQuery: "redux",
+      text: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(response => response.json())
+      .then(data => this.setState({ redditData: data.data.children }));
+  }
+
   render() {
+    let reg = /^\[/;
+    const threads = this.state.redditData.filter(soccer =>
+      reg.test(soccer.data.title)
+    );
+    let d = new Date();
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {/*<h2> Example Shit </h2>
+        <ol>
+          {this.state.redditData.map(hit => (
+            <li key={hit.data.created_utc}>
+              <a href={hit.data.url}>{hit.data.title}</a>
+            </li>
+          ))}
+        </ol>*/}
+        <br />
+        <h2
+          onClick={() => window.open("https://campusstreams.ga")}
+          className="weather"
+        >
+          {" "}
+          Soccer Games for {days[d.getDay()]}, {months[d.getMonth()]}{" "}
+          {d.getDate()}
+        </h2>
+        <ol className="center">
+          {threads.map(hit => (
+            <li key={hit.data.created_utc}>
+              <a href={hit.data.url} target="_blank">
+                {hit.data.title}
+              </a>
+
+              <div className="italicize">soccer is tight</div>
+            </li>
+          ))}
+        </ol>
+        <div> </div>
       </div>
     );
   }
