03e18edc8c9838f672dd92f5d243868c0c07f555
Jacob Ingalls <jingalls@uoregon.edu>
2019-03-18 23:10:17 -0700

    Updates

---
+++ precache-manifest.c8cf40d5cccd8479d446810c0966c1f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d2de7ad85ca5ee792f4",
+    "url": "/soccerreact/static/js/runtime~main.a668fb11.js"
+  },
+  {
+    "revision": "f7f2d5d3407edfc8d503",
+    "url": "/soccerreact/static/js/main.f16261ea.chunk.js"
+  },
+  {
+    "revision": "e93bc7dff72ddddbd85a",
+    "url": "/soccerreact/static/js/2.a81b9cc6.chunk.js"
+  },
+  {
+    "revision": "f7f2d5d3407edfc8d503",
+    "url": "/soccerreact/static/css/main.0db757fb.chunk.css"
+  },
+  {
+    "revision": "a7c3d1e879fa11bccdf1580cb818c7f0",
+    "url": "/soccerreact/index.html"
+  }
+];
